532560b400637ca591ceafa75112e9364e26191f
unknown <kravets16@gmail.com>
2023-02-21 20:20:48 +0200

    add hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,21 @@
 /* menu */
-/* window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__list'),
+    menuItem = document.querySelectorAll('.nav__list-item'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger-active');
+        menu.classList.toggle('nav__list-active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+            hamburger.classList.toggle('hamburger-active');
+            menu.classList.toggle('nav__list-active');
         })
     })
-}) */
+})
 
 const swiper = new Swiper('.hero__swiper', {
     slidesPerView: 1,
