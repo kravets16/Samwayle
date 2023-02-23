8b1990defd43a37b60bb6af200ab6929e8ac1b05
unknown <kravets16@gmail.com>
2023-02-23 21:07:24 +0200

    add menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,3 +27,30 @@ const swiper = new Swiper('.hero__swiper', {
         nextEl: '.swiper-button-next',
     },
 });
+
+const swiper2 = new Swiper('.menu__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    effect: 'slide',
+    speed: 400,
+    spaceBetween: 30,
+    breakpoints: {
+        576: {
+          slidesPerView: 1.3,
+          spaceBetween: 20,
+          centeredSlides: true,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+    },
+    /* navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }, */
+});
